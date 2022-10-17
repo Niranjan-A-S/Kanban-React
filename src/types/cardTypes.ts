821b3e388dcbd40354b49d6e70edb14b20fa7eff
Niranjan <niranjan0881@gmail.com>
2022-10-17 14:38:21 +0530

    refactor

--- src/types/cardTypes.ts
+++ src/types/cardTypes.ts
@@ -15,30 +15,3 @@ export interface ICardContextType {
   onSort: (sortValue: string) => void;
   setDisplay: (displayForm: boolean) => void;
 }
-
-export enum CardStateColors {
-  REQUESTED = "rgb(80, 123, 204)",
-  INPROGRESS = "rgb(248, 180, 69)",
-  COMPLETED = "rgb(75, 196, 86)",
-}
-
-export enum CardStates {
-  REQUESTED = "requested",
-  INPROGRESS = "in-progress",
-  COMPLETED = "completed",
-}
-
-export enum CardTypeColors {
-  FEATURE = "#06ff00",
-  ENHANCEMENT = "#4b7be5",
-  BUG = "#ff1818",
-}
-
-export enum CardSortCriterion {
-  HIGHTOLOW = "highToLow",
-  LOWTOHIGH = "lowToHigh",
-}
-
-export const ItemType = {
-  CARD: "card",
-};
